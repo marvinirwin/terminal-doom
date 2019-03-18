94fe628f54665916aaf41992cdd142065de98a1f
Marvin Irwin <marvin@marvinirwin.com>
2019-03-18 05:26:09 -0700

    is there files here?
    Former-commit-id: d24042fa2cc20059dd2cdb0d32950556e65e8a14

--- doom--0.0.1.sql
+++
@@ -1,6 +0,0 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION doom" to load this file. \quit
-CREATE FUNCTION doom() RETURNS text
-AS '$libdir/doom'
-LANGUAGE C IMMUTABLE STRICT;
-

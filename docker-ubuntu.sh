aa5c5fb0070a0f456378de7572913e469fa73044
Marvin Irwin <marvin@marvinirwin.com>
2019-05-23 07:11:22 -0700

    Builds and runs on macos, but not ubuntu?  Error is TROO frame A has rotations and a rot=0 lump

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-
-cd ..
-
 sudo docker rm DOOM_UBUNTU
-# I think this command should create a volume with the host path as the current directory
+
 sudo docker run --name=DOOM_UBUNTU -it \
     -v $(pwd)/build:/terminal-doom \
-    -v /Users/frodo/CLionProjects/test-memfs:/test \
     i386/ubuntu:xenial
-
 
 sudo docker exec -it DOOM_UBUNTU /bin/bash
 

dbe7a873d69e1a7eeaa4398ccef79a9d4c6de5e3
Marvin Irwin <marvin@marvinirwin.com>
2019-05-23 09:02:22 -0700

    docker script works, it compiles, but errors out on execution, probably because gcc

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -3,16 +3,12 @@ sudo docker rm DOOM_UBUNTU
 
 sudo docker run --name=DOOM_UBUNTU -it \
     -v $(pwd)/build:/terminal-doom \
-    i386/ubuntu:xenial
+    i386/ubuntu:xenial /bin/bash -c "
+        apt update;
+        apt-get -y install libncurses5-dev cmake;
+        export TERM=xterm;
+        cd /terminal-doom;
+        cmake .;
+        make;
+    "
 
-sudo docker exec -it DOOM_UBUNTU /bin/bash
-
-apt-get install libncurses5-dev cmake
-
-ls /terminal-doom
-
-export TERM=xterm
-
-cd /terminal-doom
-cmake .
-make

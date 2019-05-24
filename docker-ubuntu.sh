d6a4a21c56e95ef134cc70674ed7ec66c36f0c4b
Marvin Irwin <marvin@marvinirwin.com>
2019-05-24 05:22:25 -0700

    remote debugger working

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
-sudo docker rm DOOM_UBUNTU
+sudo docker rm DOOM_UBUNTU;
 
 sudo docker run --name=DOOM_UBUNTU -it \
     -v $(pwd)/build:/terminal-doom \
+    --expose=22 \
+    -p 22:22/tcp \
     i386/ubuntu:xenial /bin/bash -c "
-        apt update;
-        apt-get -y install libncurses5-dev cmake;
-        export TERM=xterm;
-        cd /terminal-doom;
-        cmake .;
-        make;
+        apt update &&
+        apt-get -y install libncurses5-dev cmake gdb &&
+        apt-get -y install openssh-server &&
+        mkdir /var/run/sshd &&
+        echo 'root:THEPASSWORDYOUCREATED' | chpasswd &&
+        sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config &&
+        sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd &&
+        export NOTVISIBLE="in users profile" &&
+        echo \"export VISIBLE=now\" >> /etc/profile &&
+        /usr/sbin/sshd  &&
+        export TERM=xterm &&
+        cd /terminal-doom &&
+        mkdir remote-host
+        cmake . &&
+        make &&
     "
 

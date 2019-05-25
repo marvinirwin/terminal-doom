139b819853e7cd1fc323f91bfcc1dc118b59c353
Marvin Irwin <marvin@marvinirwin.com>
2019-05-25 10:11:42 -0700

    shell script works!

--- docker-ubuntu.sh
+++ docker-ubuntu.sh
@@ -2,28 +2,28 @@
 sudo docker rm DOOM_UBUNTU;
 
 sudo docker run --name=DOOM_UBUNTU -it \
-    --expose=22 \
-    --expose=1234 \
-    -p 22:22/tcp \
-    -p 1234:1234/tcp \
-    --security-opt seccomp:unconfined \
     -v $(pwd)/build:/terminal-doom \
     i386/ubuntu:xenial /bin/bash -c "
         apt update &&
-        apt-get -y install libncurses5-dev cmake gdb vim &&
-        apt-get -y install openssh-server &&
-        mkdir /var/run/sshd &&
-        echo 'root:THEPASSWORDYOUCREATED' | chpasswd &&
-        echo "AllowTCPForwarding yes" >> /etc/ssh/sshd_config &&
-        echo "PermitOpen any" >> /etc/ssh/sshd_config &&
-        sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config &&
-        sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd &&
-        /usr/sbin/sshd  &&
-        export TERM=xterm &&
+        apt-get -y install libncurses5-dev cmake  &&
         cd /terminal-doom &&
         cmake . &&
         make &&
-        gdbserver :1234 ./terminal-doom
+        ./terminal-doom
     "
-
+    # --security-opt seccomp:unconfined \
+    # --expose=22 \
+    # --expose=1234 \
+    # -p 22:22/tcp \
+    # -p 1234:1234/tcp \
+        # gdb vim
+        # apt-get -y install openssh-server &&
+        # mkdir /var/run/sshd &&
+        # echo 'root:THEPASSWORDYOUCREATED' | chpasswd &&
+        # echo "AllowTCPForwarding yes" >> /etc/ssh/sshd_config &&
+        # echo "PermitOpen any" >> /etc/ssh/sshd_config &&
+        # sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config &&
+        # sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd &&
+        # /usr/sbin/sshd  &&
+        # export TERM=xterm &&
 
